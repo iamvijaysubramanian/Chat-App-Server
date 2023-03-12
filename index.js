332f5eccd88e4168d5a8efd70fdc02d2cbf5dec5
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-03-12 14:41:26 +0530

    cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://astonishing-kataifi-521af8.netlify.app/',
         methods: ['GET', 'POST']
     }
 })
@@ -28,7 +28,7 @@ db();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://astonishing-kataifi-521af8.netlify.app/',
     credentials: true
 }));
 
